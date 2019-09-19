f07f5af8bb2a2fa346e60979ec1f1ce3ec87899d
Patrick <patedm@gmail.com>
2019-09-19 14:24:46 +0100

    Clean up main v1

--- Julia-v1/main_v1.jl
+++ Julia-v1/main_v1.jl
@@ -1,4 +1,12 @@
-# v1: finite consumption saving endowment problem
+## Simple Life Cycle Model of Consumption and Savings
+## Based off of matlab code by Monica Costa Dias and Cormac O'Dea
+
+# Model v1: finite consumption saving endowment problem
+# Model v3: add deterministic income stream
+# Model v5: realistic income uncertainty
+
+# This code was written in Julia v0.6.2 in September 2017.
+# This code is designed to be easy to understand, not to be as fast as possible
 
 using Interpolations
 using Optim
@@ -16,8 +24,8 @@ const beta = 1/(1+r)              # Discount factor
 const gamma = 1.5                 # Coefficient of relative risk aversion
 const gamma_mod = 1.0-gamma       # For speed, just do this once
 const startA = 1                  # How much asset do people start life with
-const numPointsA = 100             # number of points in the discretised asset grid
-gridMethod = "5logsteps"         # method to construct grid. One of equalsteps or 5logsteps
+const numPointsA = 100            # number of points in the discretised asset grid
+gridMethod = "5logsteps"          # method to construct grid. One of equalsteps or 5logsteps
 
 # GET ASSET GRID
 # populate grid for assets using 'gridMethod'
