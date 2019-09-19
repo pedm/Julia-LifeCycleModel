c62a74b5b96b97ff329cefed629b82f54114762a
Patrick <patedm@gmail.com>
2019-09-19 14:23:34 +0100

    Clean up

--- Julia-v5/main_v5.jl
+++ Julia-v5/main_v5.jl
@@ -1,27 +1,12 @@
 ## Simple Life Cycle Model of Consumption and Savings
-## Based off of matlab code by Monica Costa-Dias and Cormac O'Dea
+## Based off of matlab code by Monica Costa Dias and Cormac O'Dea
 
-# v1: finite consumption saving endowment problem
-# v3: add deterministic income stream
-# v5: realistic income uncertainty
+# Model v1: finite consumption saving endowment problem
+# Model v3: add deterministic income stream
+# Model v5: realistic income uncertainty
 
-################################################################################
-# Euler Equation Update (Jan 2019)
-
-# Next steps:
-# 1) add the option to linearize the slope of EdU when using EE (as in Cormac's code) -- HD working on it
-# 2) for a given individual, plot the euler equation over time. is it flat?
-# 3) plot marginal utility of consumption by A1 (given some income)
-# 4) for some arbitrary set of states, can we plot eulerforzero() across A1? then decompose this into the
-# 5) modify income process so that you get less income when young. turn on borrowing, check that households consumption smooth by borrowing when young. turn off borrowing, check that euler equation is violated when young due to credit constraints
-
-# Way down the road
-# 1) create a higher interest rate for borrowing than lending, solve with EE method (will require two EEs, as explained in KV2014 appendix)
-# 2) allow for a discrete choice in the model, with some sort of smoothing, and solve with euler equation (following Blundell, Costa-Dias, Meghir, and Shaw)
-
-# note that consumption in retirement is much smoother when solving using euler equation
-# why does consumption go up in retirement when 30 asset points?
-# did tony smith suggest a faster way to find roots when using interpolation?
+# This code was written in Julia v0.6.2 in September 2017.
+# This code is designed to be easy to understand, not to be as fast as possible
 
 ################################################################################
 ## Run parallel or not
@@ -54,12 +39,12 @@ catch
 	Pkg.add("Interpolations")
 	Pkg.add("Optim")
 	Pkg.add("Roots")
-    Pkg.add("QuadGK")
+    	Pkg.add("QuadGK")
 
 	@everywhere using Interpolations
 	@everywhere using Optim
 	using Roots
-    using QuadGK
+    	using QuadGK
 end
 
 include("src/modelSetup.jl")
@@ -98,14 +83,14 @@ const T                    = 60                  # Number of time period
 const Tretire              = 45                  # Age at which retirement happens
 const borrowingAllowed     = 0                   # allow borrowing
 const isUncertainty        = 1                   # uncertain income (currently: only works if isUncertainty == 1)
-const numPointsY           = 10                  # number of points in the income grid
-const numPointsA           = 100                 # number of points in the discretised asset grid
+const numPointsY           = 9                   # number of points in the income grid
+const numPointsA           = 50                  # number of points in the discretised asset grid
 const gridMethod           = "5logsteps"         # method to construct grid. One of equalsteps or 5logsteps
 const normBnd              = 3                   # truncate the normal distrib: ignore draws less than -NormalTunc*sigma and greater than normalTrunc*sigma
 const numSims              = 10                  # How many individuals to simulate
-const useEulerEquation     = true                # Solve the model using the euler equation?
+const useEulerEquation     = false               # Solve the model using the euler equation?
 const saveValue_inEE       = false               # When using euler equation to solve the model, do we want to compute EV? (Note: adds time due to interpolation)
-const linearise            = false                # Whether to linearise the slope of EdU when using EE
+const linearise            = false               # Whether to linearise the slope of EdU when using EE
 
 ################################################################################
 ## Setup Model
@@ -137,7 +122,7 @@ else
     @time policyA1, policyC, V, EV  = solveValueFunctionPar(params, Agrid, Ygrid, incTransitionMrx)
 end
 
-## NOTE: evaluation time will be faster if you run it a second time, due to just in time compilation
+# NOTE: evaluation time will be faster if you run it a second time, due to just in time compilation
 
 ################################################################################
 ## Simulate
@@ -157,10 +142,8 @@ catch
 	using Plots
 end
 
-# http://docs.juliaplots.org/latest/
-plotly() # this works better for development -- shows the plot in firefox. but it does not allow you to save as a pdf or png
-# gr() # this works better on the server -- does not display the plot, but allows you to save it
-# or try with plotlyjs()
+plotly() # this works better for development -- shows the plot in firefox
+# gr() # this works better on the server
 
 plotCpath(cpath)
 # savefig("TEST_cpath.pdf")
@@ -173,30 +156,3 @@ plotApath(apath, MinAss)
 
 plotYCAndApaths( ypath, cpath, apath );
 
-
-################################################################################
-## Profile
-################################################################################
-
-# Next steps
-# TODO: get simNoUncer() working in this version
-# TODO: dont use zeros(), instead use a = Array(Float64, size)
-# TODO: use profiling:
-
-## Tools to optimize code:
-# @timev solveValueFunction(params, Agrid, Ygrid, incTransitionMrx);
-# @code_warntype solveValueFunction(params, Agrid, Ygrid, incTransitionMrx);
-
-# Profile.clear()  # in case we have any previous profiling data
-# @profile solveValueFunction(params, Agrid, Ygrid, incTransitionMrx)
-# Profile.print()
-
-# try
-# 	using ProfileView
-# catch
-# 	println("Installing packages")
-# 	Pkg.add("ProfileView")
-# 	using ProfileView
-# end
-#
-# ProfileView.view()
