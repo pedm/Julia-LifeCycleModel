7c3f726091d7d84b4b95e7db41be6b5cc461a8b7
Patrick <patedm@gmail.com>
2023-09-28 17:22:33 -0400

    Revert "Loop over possible illiq assets (ixB) without doing anything"
    This reverts commit 0f3cad1bd4527b492cf5b1b7665fa64a6bc50156.

--- Julia-v6-two-asset/src/solveValueFunctionPar.jl
+++ Julia-v6-two-asset/src/solveValueFunctionPar.jl
@@ -36,68 +36,58 @@ function solveValueFunctionPar(params::Dict{String,Float64}, Agrid, Bgrid, Ygrid
 
         # STEP 1. Inner problem: solve for consumption assuming that the household does not adjust illiquid assets
         # ---------------------------------------------------------
-        Threads.@threads for ixB0 = 1:1:numPointsB      # points on illiq asset grid
+        Threads.@threads for ixB = 1:1:numPointsB       # points on illiq asset grid
+
             for ixY = 1:numPointsY                      # points on income grid
 
-                # define interpolation function itp - note does not depend on ixA0
+                # define interpolation function itp - note does not depend on ixA
                 nodes = (Agrid1,)
-                EV1  = EV[ixt + 1, :, ixB0, ixY]         # relevant section of EV matrix (in assets tomorrow)
+                EV1  = EV[ixt + 1, :, ixB, ixY]         # relevant section of EV matrix (in assets tomorrow)
                 itp = interpolate(nodes, EV1, Gridded(Linear()))
                 
-                # maybe better to define Lgrid here and then loop over ixL0
-                for ixA0 = 1:numPointsA               # points on liq asset grid
+                for ixA = 1:numPointsA               # points on liq asset grid
 
                     # Value of income and information for optimisation
-                    A0    = Agrid[ixt, ixA0]            # assets today
-                    Y0    = Ygrid[ixt, ixY]            # income today
+                    A    = Agrid[ixt, ixA]            # assets today
+                    Y    = Ygrid[ixt, ixY]            # income today
                     lbA1 = Agrid[ixt + 1, 1]          # lower bound: assets tomorrow
-                    ubA1 = (A0 + Y0 - minCons)*(1.0+r)  # upper bound: assets tomorrow
+                    ubA1 = (A + Y - minCons)*(1.0+r)  # upper bound: assets tomorrow
 
                     # Compute solution
                     if (ubA1 - lbA1 < minCons)        # if liquidity constrained
-                        negV = objectivefunc(params, itp, lbA1, A0, Y0)
-                        policyA1_inner[ixt,ixA0,ixB0,ixY] = lbA1
+                        negV = objectivefunc(params, itp, lbA1, A, Y)
+                        policyA1_inner[ixt,ixA,ixB,ixY] = lbA1
                     else
                         # Find the A1 that minimizes the objective function
 
                         # define obj fcn
                         function obj(A1::Float64)
-                            return objectivefunc(params, itp, A1, A0, Y0)
+                            return objectivefunc(params, itp, A1, A, Y)
                         end
                         Res = optimize(obj,lbA1,ubA1, abs_tol = 1e-5)          # println(Res)
-                        policyA1_inner[ixt,ixA0,ixB0,ixY] = Res.minimizer
+                        policyA1_inner[ixt,ixA,ixB,ixY] = Res.minimizer
                         negV = Res.minimum     # if interior solution
                     end # if (ubA1 - lbA1 < minCons)
 
                     # Store solution and its value
-                    policyC_inner[ixt, ixA0, ixB0, ixY] = A0 + Y0 - policyA1_inner[ixt, ixA0, ixB0, ixY]/(1.0+r)
+                    policyC_inner[ixt, ixA, ixB, ixY] = A + Y - policyA1_inner[ixt, ixA, ixB, ixY]/(1.0+r)
                     # todo: policy fcn for deposit = 0 in this case 
-                    V_inner[ixt, ixA0, ixB0, ixY]       = -negV
-                end #ixA0
+                    V_inner[ixt, ixA, ixB, ixY]       = -negV
+                end #ixA
             end #ixY
-        end # ixB0
+        end # ixB
 
-        # Basically the above gives V_inner = V^{NA}(ixt, ixA0, ixB0, ixY) which assumes no adjustment of illiquid asset
+        # Basically the above gives V_t(ixA, ixB, ixY) assuming no adjustment of illiquid asset
         # Next step: find the optimal deposit d into the illiquid asset
+        # This will reduce 
 
         # STEP 2. Outer problem to find optimal deposit function
         # Will have to interpoate V_inner over Lgrid and Bgrid 
         
-        # Create Lgrid given ixB0 and ixY
+        # Create Lgrid given ixB and ixY
         # Works because in the inner problem: a = l - c => can rearrange so that l = a + c       
-        # Lgrid = Agrid + policyC_inner[ixt, :, ixB0, ixY]
-        # But wait.... ixB0 can change?
-        # I guess can loop over ixB0 and ixY, then for each create Lgrid, then interpolate V_inner on Lgrid and Bgrid
-
-        # Threads.@threads for ixB0 = 1:1:numPointsB       # points on illiq asset grid
-        #     for ixY = 1:numPointsY                      # points on income grid
-        #         Lgrid = Agrid + policyC_inner[ixt, :, ixB0, ixY]
-
-        #     end #ixY
-        # end # ixB0
-
-        # Notes from Jeppe: 
-        # m_t+1 = R * a_t + y_t+1
+        # Lgrid = Agrid + policyC_inner[ixt, :, ixB, ixY]
+        # But wait.... ixB can change?
 
         # Or easier for VFI:
         # interpolate V_inner over Agrid and Bgrid
